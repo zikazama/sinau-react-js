a3285cf4001408cfa7924dc74ef8b54509eac162
Fauzi <Fauzi190198@gmail.com>
2024-12-04 20:32:56 +0700

    feat: add github action

---
+++ src/App.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { expect } from "vitest";
+
+test("renders learn react link", () => {
+//   const { asFragment, getByText } = render(<App />);
+//   expect(getByText("Learn React")).toBeInTheDocument();
+});
